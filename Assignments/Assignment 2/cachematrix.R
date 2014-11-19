c4c25ce93656707e73028a48280a2b0292460249
Khong Duc Thinh <ryan.khong212@gmail.com>
2014-11-20 00:15:30 +0800

    Added comments

--- Assignments/Assignment 2/cachematrix.R
+++ Assignments/Assignment 2/cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script is to generate a square matrix according to the user's
+## description and to calculate its inverse matrix. The value of the inversed  
+## matrix is stored (or cached) in setinverse function, which will return the 
+## its value if the inverse function of the same square matrix is called again 
+## (without have to re-calculate). If a new square matrix is defined, the invserse
+## function will calculate again and store in the setinverse function as the new
+## value which supersedes the old inversed matrix.
 
-## Write a short comment describing this function
+## This function is to store a matrix from the user's input
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv<-NULL
-    set<-function(y) {
-          x<<-y
+    inv<-NULL       ##The value of inv is set to NULL when a makeCacheMatrix is called
+    set<-function(y) {  ##This method is to set a new matrix to supersede the old one
+          x<<-y   ##Assign the x matrix with the new y matrix and set inv to NULL
           inv<<-NULL
     }
-    get<-function() {x}
+    get<-function() {x} ##To return the x matrix
     setinv<-function(inverse) inv<<-inverse
     getinv<-function() inv
     list(set=set, get=get, setinv=setinv, getinv=getinv)
